2fbaf70d65f465941a4d37a6bfce105ffeca1cd4
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-12-06 23:51:34 +0530

    Implemented the page structures

--- services/api.service.ts
+++ services/api.service.ts
@@ -14,18 +14,3 @@ export const apiClient = axios.create({
         "Content-Type": "application/json" // backend always needs json
     }
 })
-
-apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response.status === 401) { // if unauth then handle the redirect and clearing the memory
-            window.dispatchEvent(new CustomEvent('auth:unauthorized')); // trigger event to clear the cache
-
-            window.location.href = '/signin'; // redirects to the signin page
-        }
-
-        return Promise.reject(error); // pass the error to the component in which apiClient is used
-    }
-)
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Terms from "./Terms";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Share from "./pages/Share";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 
 
 
 export default function App() {
-  return <div>hi</div>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+      <Route path="/share/:hash" element={<Share />} />
+      <Route path="/" element={<Terms />} />
+    </Routes>
+  </BrowserRouter>
 }
